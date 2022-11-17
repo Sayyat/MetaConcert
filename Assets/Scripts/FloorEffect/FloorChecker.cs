4bf1bac17ecb0166a2347c1514f3ecf895adfc69
Corvusincvr <Corvusincvr@gmail.com>
2022-11-17 14:19:45 +0600

    Added InputSystem UI + Photon.

--- Assets/Scripts/FloorEffect/FloorChecker.cs
+++ Assets/Scripts/FloorEffect/FloorChecker.cs
@@ -5,50 +5,50 @@ using UnityEngine;
 
 public class FloorChecker : MonoBehaviour
 {
-    [SerializeField] private LivingParticleArrayController[] arrayControllers;
-
-    private void Start()
-    {
-        if (arrayControllers == null)
-        {
-            arrayControllers = GetComponents<LivingParticleArrayController>();
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (CheckLayerMask(other.gameObject, LayerMask.NameToLayer("Player")))
-        {
-            Debug.Log("Player entered");
-        }
-
-        for (int i = 0; i < arrayControllers.Length; i++)
-        {
-            arrayControllers[i].AddAffector(other.transform);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (CheckLayerMask(other.gameObject, LayerMask.NameToLayer("Player")))
-        {
-            Debug.Log("Player exited");
-        }
-
-        for (int i = 0; i < arrayControllers.Length; i++)
-        {
-            arrayControllers[i].RemoveAffector(other.transform);
-        }
-    }
-
-
-    public bool CheckLayerMask(GameObject obj, LayerMask layers)
-    {
-        if (((1 << obj.layer) & layers) != 0)
-        {
-            return true;
-        }
-
-        return false;
-    }
+    // [SerializeField] private LivingParticleArrayController[] arrayControllers;
+    //
+    // private void Start()
+    // {
+    //     if (arrayControllers == null)
+    //     {
+    //         arrayControllers = GetComponents<LivingParticleArrayController>();
+    //     }
+    // }
+    //
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (CheckLayerMask(other.gameObject, LayerMask.NameToLayer("Player")))
+    //     {
+    //         Debug.Log("Player entered");
+    //     }
+    //
+    //     for (int i = 0; i < arrayControllers.Length; i++)
+    //     {
+    //         arrayControllers[i].AddAffector(other.transform);
+    //     }
+    // }
+    //
+    // private void OnTriggerExit(Collider other)
+    // {
+    //     if (CheckLayerMask(other.gameObject, LayerMask.NameToLayer("Player")))
+    //     {
+    //         Debug.Log("Player exited");
+    //     }
+    //
+    //     for (int i = 0; i < arrayControllers.Length; i++)
+    //     {
+    //         arrayControllers[i].RemoveAffector(other.transform);
+    //     }
+    // }
+    //
+    //
+    // public bool CheckLayerMask(GameObject obj, LayerMask layers)
+    // {
+    //     if (((1 << obj.layer) & layers) != 0)
+    //     {
+    //         return true;
+    //     }
+    //
+    //     return false;
+    // }
 }
