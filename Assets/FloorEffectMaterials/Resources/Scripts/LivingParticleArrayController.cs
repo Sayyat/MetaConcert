0ba48151135deca5cc482808e7ff37ac97b77b43
Corvusincvr <Corvusincvr@gmail.com>
2022-11-18 17:36:38 +0600

    Added checker on plane dance

--- Assets/FloorEffectMaterials/Resources/Scripts/LivingParticleArrayController.cs
+++ Assets/FloorEffectMaterials/Resources/Scripts/LivingParticleArrayController.cs
@@ -2,27 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LivingParticleArrayController : MonoBehaviour {
-
-    public Transform[] affectors;
+public class LivingParticleArrayController : MonoBehaviour
+{
+    public List<Transform> affectors;
 
     private Vector4[] positions;
     private ParticleSystemRenderer psr;
 
-	void Start () {
+    void Start()
+    {
         psr = GetComponent<ParticleSystemRenderer>();
         Vector4[] maxArray = new Vector4[20];
         psr.material.SetVectorArray("_Affectors", maxArray);
     }
 
     // Sending an array of positions to particle shader
-    void Update () {
-        positions = new Vector4[affectors.Length];
+    void Update()
+    {
+        if (affectors.Count < 1)
+            return;
+
+        positions = new Vector4[affectors.Count];
         for (int i = 0; i < positions.Length; i++)
         {
             positions[i] = affectors[i].position;
         }
+
         psr.material.SetVectorArray("_Affectors", positions);
-        psr.material.SetInt("_AffectorCount", affectors.Length);
+        psr.material.SetInt("_AffectorCount", affectors.Count);
+    }
+
+    public void AddAffector(Transform otherTransform)
+    {
+       affectors.Add(otherTransform);
+    }
+
+    public void RemoveAffector(Transform otherTransform)
+    {
+        affectors.Remove(otherTransform);
     }
 }
