9f5b81b8910621f817513802d880ffb2537175f5
Nitro <sayatraykulov@gmail.com>
2022-11-19 13:20:18 +0600

    Builded on Webgl

--- Assets/Scripts/Init/InitPlayer.cs
+++ Assets/Scripts/Init/InitPlayer.cs
@@ -1,58 +1,24 @@
 using System.IO;
 using Photon.Pun;
 using StarterAssets;
-#if UNITY_EDITOR
 using UnityEditor;
-#endif
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
 public class InitPlayer : MonoBehaviourPun
 {
-    private GameObject playerPrefab;
-    private GameObject player;
 
 
-    private PhotonAnimatorView _photonAnimatorView;
-    private PhotonTransformView _photonTransformView;
-    private PhotonView _photonView;
-
-    private ThirdPersonController _controller;
-
-    private CameraControl _cameraControl;
-    private StarterAssetsInputs _starterAssetsInputs;
-    private BasicRigidBodyPush _basicRigidBodyPush;
+    [SerializeField] private GameObject playerPrefab;
+  
 
     private void Start()
     {
-        player = new GameObject("PlayerPrefab");
-
-
-        Instantiate(player, Vector3.up, Quaternion.identity);
-
-        _controller = player.AddComponent<ThirdPersonController>();
-        var targetGO = new GameObject("CameraTarget")
-        {
-            transform =
-            {
-                parent = player.transform,
-                position = new Vector3(0f, 1.66f, -0.76f)
-            }
-        };
-
-        _controller.CinemachineCameraTarget = targetGO;
-
-
-#if UNITY_EDITOR
-        if (!Directory.Exists("Assets/Resources/Dynamic"))
-            AssetDatabase.CreateFolder("Assets/Resources", "Dynamic");
-
-        string localPath = "Assets/Resources/Dynamic/" + player.name + ".prefab";
-        playerPrefab = PrefabUtility.SaveAsPrefabAsset(player, localPath, out var success);
-        Debug.Log($"Prefab created: {success}");
-#endif
-
-        // PhotonNetwork.Instantiate($"Players/{playerPrefab.name}", Vector3.up, Quaternion.identity );
+        
+        var player = PhotonNetwork.Instantiate("PlayerTemplate", Vector3.up, Quaternion.identity );
+        
+       
+        
     }
 }
