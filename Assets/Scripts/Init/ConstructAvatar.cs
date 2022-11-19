ab456bfe533c47112b635fe0c0765701ffc3254c
Nitro <sayatraykulov@gmail.com>
2022-11-19 17:29:28 +0600

    ReadyPlayerMeAPI added control fixed (Levitating problem)

---
+++ Assets/Scripts/Init/ConstructAvatar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Init
+{
+    [RequireComponent(typeof(Animator))]
+    public class ConstructAvatar : MonoBehaviour, IPunInstantiateMagicCallback
+    {
+      
+        private InitPlayer _initPlayer;
+        private Animator _animator;
+
+        private void Awake()
+        {
+          _animator = this.GetComponent<Animator>();
+            var initPlayer = GameObject.Find("InitPlayer");
+            _initPlayer = initPlayer.GetComponent<InitPlayer>();
+        }
+
+        public void OnPhotonInstantiate(PhotonMessageInfo info)
+        {
+            _initPlayer.Avatar = this;
+        }
+        
+        public void Construct(GameObject playerOnScene)
+        {
+            var child1 = playerOnScene.transform.GetChild(0);
+            var child2 = playerOnScene.transform.GetChild(1);
+            
+//todo Add player layer
+            child1.transform.parent = transform;
+            child2.transform.parent = transform;
+            
+            child1.transform.position = Vector3.zero;
+            child2.transform.position = Vector3.zero;
+        }
+
+        public void SetupAvatarOnAnimator(Avatar avatarScheme)
+        {
+            _animator.avatar = avatarScheme;
+        }
+    }
+}
--- Assets/Scripts/Init/InitPlayer.cs
+++ Assets/Scripts/Init/InitPlayer.cs
@@ -1,24 +1,34 @@
-using System.IO;
+using System;
 using Photon.Pun;
-using StarterAssets;
-using UnityEditor;
-using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
+using Init;
+using UnityEngine;
 
 public class InitPlayer : MonoBehaviourPun
 {
+    [SerializeField] private Avatar avatarSchemeMan;
+    [SerializeField] private Avatar avatarSchemeWoman;
+    [SerializeField] private GameObject readyPlayerPrefab;
+    private GameObject readyPlayer;
 
-
-    [SerializeField] private GameObject playerPrefab;
-  
+    private bool IsConstructed = false;
+    public ConstructAvatar Avatar { get; set; }
 
     private void Start()
     {
-        
-        var player = PhotonNetwork.Instantiate("PlayerTemplate", Vector3.up, Quaternion.identity );
-        
-       
-        
+        PhotonNetwork.Instantiate("PlayerTemplate", Vector3.up, Quaternion.identity);
+        readyPlayer = Instantiate(readyPlayerPrefab);
+    }
+
+    private void Update()
+    {
+        if (Avatar == null || IsConstructed)
+            return;
+
+        Avatar.Construct(readyPlayer);
+        Avatar.SetupAvatarOnAnimator(avatarSchemeMan);
+        Destroy(readyPlayer);
+        IsConstructed = true;
     }
 }
