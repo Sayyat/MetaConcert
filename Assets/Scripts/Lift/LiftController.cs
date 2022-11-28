e94982cad8ec9ee51b4ad11ac8e70e57d0f8f023
Nitro <sayatraykulov@gmail.com>
2022-11-28 12:17:14 +0600

    created Lift prefab

--- Assets/Scripts/Lift/LiftController.cs
+++ Assets/Scripts/Lift/LiftController.cs
@@ -1,10 +1,75 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LiftController : MonoBehaviour
+namespace Lift
 {
-    [SerializeField] private GameObject platform;
-    [SerializeField] private GameObject buttons;
-    
+    public class LiftController : MonoBehaviour
+    {
+        [SerializeField] private GameObject platform;
+
+        [Header("Spots were lift can stop")] [SerializeField]
+        private List<Transform> liftStopSpots;
+
+        [Header("Buttons to call lift")] [SerializeField]
+        private List<GameObject> liftCallButtons;
+        
+        [Header("Buttons inside the lift")] [SerializeField]
+        private List<GameObject> floorButtons;
+
+        private Animation _platformAnimation;
+
+        private LiftStates _liftStates;
+
+        private List<Transform> _liftCallButtonTransforms;
+        private List<Transform> _floorButtonTransforms;
+
+        private void Awake()
+        {
+            _liftStates = GetComponent<LiftStates>();
+            liftCallButtons.ForEach(button =>
+            {
+                _liftCallButtonTransforms.Add(button.transform);
+            });
+            
+            floorButtons.ForEach(button =>
+            {
+                _floorButtonTransforms.Add(button.transform);
+            });
+        }
+
+        private void Start()
+        {
+            
+        }
+
+
+      
+        
+        void Update() {  
+            if (Input.GetMouseButtonDown(0)) {  
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  
+                RaycastHit hit;  
+                if (Physics.Raycast(ray, out hit)) {  
+                    //Select stage    
+                    if (_liftCallButtonTransforms.Contains(hit.transform)) {
+                        Debug.Log("<Color=Red>liftCallButton pressed</Color>");
+                    }  
+                    if (_floorButtonTransforms.Contains(hit.transform)) {
+                        Debug.Log("<Color=Red>floorButtons pressed</Color>");
+                    }  
+
+                    
+                }  
+            }  
+        }  
+
+        private void MoveLift(Vector3 targetPosition)
+        {
+            Debug.Log("<Color=Red>Lift started moving</Color>");
+            platform.transform.position =
+                Vector3.MoveTowards(platform.transform.position, targetPosition,0.02f * Time.deltaTime);
+        }
+    }
 }
---
+++ Assets/Scripts/Lift/LiftStates.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Lift
+{
+    public class LiftStates : MonoBehaviour
+    {
+        public int Floor { get; set; } = 0;
+        public bool IsMoving { get; set; } = false;
+        
+    }
+}
