0fad7c5ddb6bba918fe64adfa58c8ca6ac18d710
Nitro <sayatraykulov@gmail.com>
2022-11-30 14:16:30 +0600

    Added colliders to second floor

--- Assets/Scripts/Lift/LiftController.cs
+++ Assets/Scripts/Lift/LiftController.cs
@@ -8,7 +8,7 @@ namespace Lift
     public class LiftController : MonoBehaviour
     {
         [SerializeField] private GameObject platform;
-
+        
         [Header("Spots were lift can stop")] [SerializeField]
         private List<Transform> liftStopSpots;
 
@@ -24,18 +24,19 @@ namespace Lift
         private List<Vector3> _floorButtonTransforms = new List<Vector3>();
 
 
-
         private Vector3 _targetPosition { get; set; }
+        private int _floor;
+        private PlatformController _platformController;
 
         private void Awake()
         {
-            _liftStates = GetComponent<LiftStates>();
+            _liftStates = new LiftStates();
             _targetPosition = liftStopSpots[0].transform.position;
+            _platformController = platform.GetComponent<PlatformController>();
         }
 
         private void Start()
         {
-
             string l = "<Color=Green>\n";
 
             for (int i = 0; i < liftCallButtons.Count; i++)
@@ -46,12 +47,8 @@ namespace Lift
             }
 
             l += "</Color>";
-            
+
             // Debug.Log(l);
-
-
-
-
         }
 
         private void Update()
@@ -60,28 +57,44 @@ namespace Lift
         }
 
 
-        // private void OnDisable()
-        // {
-        //     liftCallButtons.ForEach(button =>
-        //     {
-        //         button.ButtonClicked -= OnButtonClicked;
-        //     });
-        //     floorButtons.ForEach(button =>
-        //     {   
-        //         button.ButtonClicked -= OnButtonClicked;
-        //     });
-        // }
+        private void OnDestroy()
+        {
+            liftCallButtons.ForEach(button => { button.ButtonClicked -= OnButtonClicked; });
+            floorButtons.ForEach(button => { button.ButtonClicked -= OnButtonClicked; });
+        }
+
         private void OnButtonClicked(string where, int floor)
         {
             Debug.Log($"<Color=Yellow>Button clicked: {where}: {floor}</Color>");
             _targetPosition = liftStopSpots[floor - 1].position;
+            _liftStates.IsMoving = true;
+            _floor = floor;
+            
+            _platformController.AddPlayerChild();
         }
 
         private void MoveLift()
         {
-            // Debug.Log("<Color=Red>Lift started moving</Color>");
+            if (!_liftStates.IsMoving)
+            {
+                _platformController.RemovePlayerChild();
+                return;
+            }
+
+            if (Vector3.Distance(platform.transform.position, _targetPosition) < 0.01)
+            {
+                _platformController.RemovePlayerChild();
+                _liftStates.IsMoving = false;
+                _liftStates.Floor = _floor;
+                return;
+            }
+
+
+            Debug.Log("<Color=Red>Lift started moving</Color>");
             platform.transform.position =
                 Vector3.MoveTowards(platform.transform.position, _targetPosition, 2f * Time.deltaTime);
         }
+
+
     }
 }
---
+++ Assets/Scripts/Lift/PlatformController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lift
+{
+    public class PlatformController : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> childPlayers = new List<GameObject>();
+        [SerializeField] private GameObject playersParent;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("<Color=Green>Collision enter</Color>");
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                childPlayers.Add(other.gameObject);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Debug.Log("<Color=Red>Collision exit</Color>");
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                childPlayers.Remove(other.gameObject);
+            }
+        }
+
+        
+        
+        public void AddPlayerChild()
+        {
+            foreach (var childPlayer in childPlayers)
+            {
+                childPlayer.transform.parent = playersParent.transform;
+            }
+        }
+
+        public void RemovePlayerChild()
+        {
+            foreach (var childPlayer in childPlayers)
+            {
+                childPlayer.transform.parent = null;
+            }
+        }
+    }
+}
