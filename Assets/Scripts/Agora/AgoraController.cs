9384f4d736de15ac75aefa3cd41934336e84f2e3
Corvusincvr <Corvusincvr@gmail.com>
2022-12-04 14:16:35 +0600

    Add agora video quad on simple scene.

---
+++ Assets/Scripts/Agora/AgoraController.cs
@@ -0,0 +1,456 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using agora_gaming_rtc;
+using agora_utilities;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public class AgoraController
+{
+    // instance of agora engine
+
+    private readonly GameObject _videoObject;
+    public AgoraController(GameObject videoObject)
+    {
+        _videoObject = videoObject;
+    }
+
+    private IRtcEngine mRtcEngine { get; set; }
+    private string ChannelName { get; set; }
+
+    private CLIENT_ROLE_TYPE ClientRole { get; set; }
+
+    // private Text ChannelNameLabel { get; set; }
+
+    private const float Offset = 100;
+
+    private bool _testVolumeIndication = false;
+
+    private List<GameObject> remoteUserDisplays = new List<GameObject>();
+    protected Dictionary<uint, VideoSurface> UserVideoDict = new Dictionary<uint, VideoSurface>();
+
+    private bool _pubVideo;
+    private bool _subVideo;
+    private bool _pubAudio;
+    private bool _subAudio;
+
+    public void LoadEngine(string appId)
+    {
+        // start sdk
+        Debug.Log("initializeEngine");
+
+        if (mRtcEngine != null)
+        {
+            Debug.Log("Engine exists. Please unload it first!");
+            return;
+        }
+
+        // init engine
+        mRtcEngine = IRtcEngine.GetEngine(appId);
+
+        // enable log
+        mRtcEngine.SetLogFilter(LOG_FILTER.DEBUG | LOG_FILTER.INFO | LOG_FILTER.WARNING | LOG_FILTER.ERROR |
+                               LOG_FILTER.CRITICAL);
+    }
+
+    public void JoinAudience(string channelName)
+    {
+        Debug.Log("calling join (channel = " + channelName + ")");
+
+        // TMPText = mRtcEngine.ToString();
+
+        if (mRtcEngine == null)
+            return;
+        // set callbacks (optional)
+        mRtcEngine.OnJoinChannelSuccess += onJoinChannelSuccess;
+        mRtcEngine.OnUserJoined += onUserJoined;
+        mRtcEngine.OnUserOffline += onUserOffline;
+        mRtcEngine.OnLeaveChannel += OnLeaveChannelHandler;
+        mRtcEngine.OnWarning = (int warn, string msg) =>
+        {
+            Debug.LogWarningFormat("Warning code:{0} msg:{1}", warn, IRtcEngine.GetErrorDescription(warn));
+        };
+        mRtcEngine.OnError += HandleError;
+        mRtcEngine.OnClientRoleChanged += handleOnClientRoleChanged;
+        mRtcEngine.OnClientRoleChangeFailed += OnClientRoleChangeFailedHandler;
+        mRtcEngine.SetChannelProfile(CHANNEL_PROFILE.CHANNEL_PROFILE_LIVE_BROADCASTING);
+        mRtcEngine.SetClientRole(CLIENT_ROLE_TYPE.CLIENT_ROLE_AUDIENCE);
+        mRtcEngine.EnableVideo();
+        mRtcEngine.EnableVideoObserver();
+        mRtcEngine.JoinChannelByKey("", channelName);
+        ChannelName = channelName;
+        ClientRole = CLIENT_ROLE_TYPE.CLIENT_ROLE_AUDIENCE;
+    }
+
+
+    private void onJoinChannelSuccess(string channelName, uint uid, int elapsed)
+    {
+        Debug.Log("JoinChannel " + channelName + " Success: uid = " + uid);
+
+        var textVersionGameObject = GameObject.Find("VersionText");
+        if (textVersionGameObject)
+        {
+            textVersionGameObject.GetComponent<Text>().text = "SDK Version : " + IRtcEngine.GetSdkVersion();
+        }
+
+        // ChannelNameLabel.text = channelName;
+    }
+
+    private void onUserJoined(uint uid, int elapsed)
+    {
+        Debug.Log("onUserJoined: uid = " + uid + " elapsed = " + elapsed);
+        // this is called in main thread
+
+        // TMPText = mRtcEngine.ToString();
+        // find a game object to render video stream from 'uid'
+        var go = GameObject.Find(uid.ToString());
+        if (!ReferenceEquals(go, null))
+        {
+            // TMPText = "TWO UID";
+            return; // reuse
+        }
+
+        // create a GameObject and assign to this new user
+        var videoSurface = MakeQuadSurface(uid.ToString());
+        if (!ReferenceEquals(videoSurface, null))
+        {
+            // configure videoSurface
+            videoSurface.SetForUser(uid);
+            videoSurface.SetEnable(true);
+            videoSurface.SetVideoSurfaceType(AgoraVideoSurfaceType.Renderer);
+
+            remoteUserDisplays.Add(videoSurface.gameObject);
+            UserVideoDict[uid] = videoSurface;
+        }
+
+        if (videoSurface != null)
+        {
+            // TMPText = "Exist VS";
+            // TMPText = videoSurface.transform.position.ToString();
+            // TMPText = videoSurface.transform.localPosition.ToString();
+        }
+    }
+
+    private void onUserOffline(uint uid, USER_OFFLINE_REASON reason)
+    {
+        // remove video stream
+        Debug.Log("onUserOffline: uid = " + uid + " reason = " + reason);
+        // this is called in main thread
+        GameObject go = GameObject.Find(uid.ToString());
+        if (!ReferenceEquals(go, null))
+        {
+            Object.Destroy(go);
+        }
+    }
+
+    private void OnLeaveChannelHandler(RtcStats stats)
+    {
+        Debug.LogFormat("OnLeaveChannelSuccess ---- duration = {0} txVideoBytes:{1} ", stats.duration,
+            stats.txVideoBytes);
+        // Clean up the displays
+        foreach (var go in remoteUserDisplays)
+        {
+            GameObject.Destroy(go);
+        }
+
+        remoteUserDisplays.Clear();
+    }
+
+    public void Join(string channelName, string token, bool enableVideo, bool muted = false)
+    {
+        Debug.Log("calling join (channel = " + channelName + ")");
+
+        // TMPText = mRtcEngine.ToString();
+
+        if (mRtcEngine == null)
+            return;
+
+        // SetupInitState();
+
+        // set callbacks (optional)
+        mRtcEngine.OnJoinChannelSuccess += onJoinChannelSuccess;
+        mRtcEngine.OnUserJoined = onUserJoined;
+        mRtcEngine.OnUserOffline += onUserOffline;
+        mRtcEngine.OnLeaveChannel += OnLeaveChannelHandler;
+        mRtcEngine.OnWarning += (int warn, string msg) =>
+        {
+            Debug.LogWarningFormat("Warning code:{0} msg:{1}", warn, IRtcEngine.GetErrorDescription(warn));
+        };
+        mRtcEngine.OnError += HandleError;
+
+        mRtcEngine.OnUserMutedAudio += OnUserMutedAudio;
+        mRtcEngine.OnUserMuteVideo += OnUserMutedVideo;
+        //   mRtcEngine.OnVolumeIndication = OnVolumeIndicationHandler;
+        mRtcEngine.OnClientRoleChanged += handleOnClientRoleChanged;
+        mRtcEngine.OnClientRoleChangeFailed += OnClientRoleChangeFailedHandler;
+        mRtcEngine.OnVideoSizeChanged += OnVideoSizeChangedHandler;
+
+        mRtcEngine.SetChannelProfile(CHANNEL_PROFILE.CHANNEL_PROFILE_LIVE_BROADCASTING);
+        mRtcEngine.SetClientRole(CLIENT_ROLE_TYPE.CLIENT_ROLE_BROADCASTER);
+
+        // Turn this on to receive volumenIndication
+        if (_testVolumeIndication)
+        {
+            mRtcEngine.EnableAudioVolumeIndication(500, 8, report_vad: true);
+        }
+
+        var _orientationMode = ORIENTATION_MODE.ORIENTATION_MODE_FIXED_LANDSCAPE;
+
+        VideoEncoderConfiguration config = new VideoEncoderConfiguration
+        {
+            orientationMode = _orientationMode,
+            degradationPreference = DEGRADATION_PREFERENCE.MAINTAIN_FRAMERATE,
+            mirrorMode = VIDEO_MIRROR_MODE_TYPE.VIDEO_MIRROR_MODE_DISABLED
+            // note: mirrorMode is not effective for WebGL
+        };
+        mRtcEngine.SetVideoEncoderConfiguration(config);
+
+        // enable video
+        if (enableVideo)
+        {
+            mRtcEngine.EnableVideo();
+            // allow camera output callback
+            mRtcEngine.EnableVideoObserver();
+        }
+        else
+        {
+            // AudioVideoState.subVideo = false;
+            // AudioVideoState.pubVideo = false;
+        }
+
+        // NOTE, we use the third button to invoke JoinChannelByKey
+        // otherwise, it joins using JoinChannelWithUserAccount
+        if (muted)
+        {
+            // mute locally only. still subscribing
+            mRtcEngine.EnableLocalAudio(false);
+            mRtcEngine.MuteLocalAudioStream(true);
+            mRtcEngine.JoinChannelByKey(channelKey: null, channelName: channelName, info: "", uid: 0);
+        }
+        else
+        {
+            // join channel with string user name
+            // ************************************************************************************* 
+            // !!!  There is incompatibiity with string Native UID and Web string UIDs !!!
+            // !!!  We strongly recommend to use uint uid only !!!!
+            // mRtcEngine.JoinChannelWithUserAccount(null, channel, "user" + Random.Range(1, 99999),
+            // ************************************************************************************* 
+            mRtcEngine.JoinChannel(
+                token: token,
+                channelId: channelName,
+                info: "",
+                uid: 0,
+                options: new ChannelMediaOptions(true, true, true, true)
+            );
+        }
+
+        ChannelName = channelName;
+        ClientRole = CLIENT_ROLE_TYPE.CLIENT_ROLE_BROADCASTER;
+        // TMPText = "END Joined";
+    }
+
+    public void Leave()
+    {
+        Debug.Log("calling leave");
+
+        if (mRtcEngine == null)
+            return;
+
+        // leave channel
+        mRtcEngine.LeaveChannel();
+        // deregister video frame observers in native-c code
+        mRtcEngine.DisableVideoObserver();
+    }
+
+    public void UnloadEngine()
+    {
+        Debug.Log("calling unloadEngine");
+
+        // delete
+        if (mRtcEngine != null)
+        {
+            IRtcEngine.Destroy(); // Place this call in ApplicationQuit
+            mRtcEngine = null;
+        }
+    }
+
+    public void OnSceneVideoLoaded()
+    {
+        
+        if (ReferenceEquals(_videoObject, null))
+        {
+            Debug.Log("failed to find Quad");
+            return;
+        }
+        else
+        {
+            _videoObject.AddComponent<VideoSurface>();
+        }
+    }
+
+    public void EnableVideo(bool paused)
+    {
+        if (mRtcEngine != null)
+        {
+            if (!paused)
+            {
+                mRtcEngine.EnableVideo();
+            }
+            else
+            {
+                mRtcEngine.DisableVideo();
+            }
+        }
+    }
+
+    public VideoSurface MakeQuadSurface(string goName)
+    {
+        var go = Object.Instantiate(Resources.Load("Quad") as GameObject);
+
+        if (go == null)
+        {
+            return null;
+        }
+
+        go.name = goName;
+
+        go.transform.Rotate(0f, 0.0f, 180.0f);
+        // var xPos = Random.Range(Offset - Screen.width / 2f, Screen.width / 2f - Offset);
+        // var yPos = Random.Range(Offset, Screen.height / 2f - Offset);
+        go.transform.localPosition = new Vector3(-1, 1.3f, -3.26f);
+
+
+        var videoSurface = go.AddComponent<VideoSurface>();
+        return videoSurface;
+    }
+
+    public VideoSurface MakeImageSurface(string goName)
+    {
+        var go = new GameObject();
+
+        if (go == null)
+        {
+            // TMPText = "NO GO";
+            return null;
+        }
+
+        go.name = goName;
+
+        // to be renderered onto
+        go.AddComponent<RawImage>();
+
+        // make the object draggable
+        go.AddComponent<UIElementDragger>();
+
+
+        GameObject canvas = GameObject.Find("Canvas");
+        // TMPText = canvas.GetComponent<RectTransform>().rect.ToString();
+
+        if (canvas == null)
+            // TMPText = "NO CANVAS";
+
+        if (canvas != null)
+        {
+            go.transform.SetParent(canvas.transform);
+        }
+
+        // set up transform
+        go.transform.Rotate(0f, 0.0f, 180.0f);
+        var xPos = Random.Range(Offset - Screen.width / 2f, Screen.width / 2f - Offset);
+        var yPos = Random.Range(Offset, Screen.height / 2f - Offset);
+        go.transform.localPosition = new Vector3(xPos, yPos, 0f);
+
+        // configure videoSurface
+        var videoSurface = go.AddComponent<VideoSurface>();
+        return videoSurface;
+    }
+
+
+    #region ChangesStats
+
+    private void OnUserMutedAudio(uint uid, bool muted)
+    {
+        Debug.LogFormat("user {0} muted audio:{1}", uid, muted);
+    }
+
+    private void OnUserMutedVideo(uint uid, bool muted)
+    {
+        Debug.LogFormat("user {0} muted video:{1}", uid, muted);
+    }
+
+    private void OnClientRoleChangeFailedHandler(CLIENT_ROLE_CHANGE_FAILED_REASON reason, CLIENT_ROLE_TYPE currentRole)
+    {
+        Debug.Log("Engine OnClientRoleChangeFaile: " + reason + " c-> " + currentRole);
+    }
+
+    void handleOnClientRoleChanged(CLIENT_ROLE_TYPE oldRole, CLIENT_ROLE_TYPE newRole)
+    {
+        Debug.Log("Engine OnClientRoleChanged: " + oldRole + " -> " + newRole);
+    }
+
+    private float EnforcingViewLength = 360f;
+
+    void OnVideoSizeChangedHandler(uint uid, int width, int height, int rotation)
+    {
+        Debug.LogWarning(string.Format("OnVideoSizeChangedHandler, uid:{0}, width:{1}, height:{2}, rotation:{3}", uid,
+            width, height, rotation));
+        if (UserVideoDict.ContainsKey(uid))
+        {
+            GameObject go = UserVideoDict[uid].gameObject;
+            Vector2 v2 = new Vector2(width, height);
+            RawImage image = go.GetComponent<RawImage>();
+            v2 = AgoraUIUtils.GetScaledDimension(width, height, EnforcingViewLength);
+
+            if (rotation == 90 || rotation == 270)
+            {
+                v2 = new Vector2(v2.y, v2.x);
+            }
+
+            image.rectTransform.sizeDelta = v2;
+        }
+    }
+
+    #endregion
+
+
+    #region Error Handling
+
+    private Text MessageText { get; set; }
+    private int LastError { get; set; }
+
+    private void HandleError(int error, string msg)
+    {
+        if (error == LastError)
+        {
+            return;
+        }
+
+        msg = string.Format("Error code:{0} msg:{1}", error, IRtcEngine.GetErrorDescription(error));
+
+        switch (error)
+        {
+            case 101:
+                msg += "\nPlease make sure your AppId is valid and it does not require a certificate for this demo.";
+                break;
+        }
+
+        Debug.LogError(msg);
+        if (MessageText != null)
+        {
+            if (MessageText.text.Length > 0)
+            {
+                msg = "\n" + msg;
+            }
+
+            MessageText.text += msg;
+        }
+
+        LastError = error;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Agora/AgoraView.cs
@@ -0,0 +1,176 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AgoraView : MonoBehaviour
+{
+    // Use this for initialization
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+    private ArrayList permissionList = new ArrayList();
+#endif
+    private static AgoraController _controller  = null;
+
+
+    // PLEASE KEEP THIS App ID IN SAFE PLACE
+    // Get your own App ID at https://dashboard.agora.io/
+    [SerializeField] private string appID = "your_appid";
+    [SerializeField] private string channelName = "your_appid";
+    [SerializeField] private string appToken = "your_token";
+    
+    public GameObject Quad { get; set; }
+    private string ChannelName
+    {
+        get
+        {
+            string cached = PlayerPrefs.GetString("ChannelName");
+            if (string.IsNullOrEmpty(cached))
+            {
+                cached = "Alem";
+            }
+
+            return cached;
+        }
+
+        set
+        {
+            PlayerPrefs.SetString("ChannelName", value);
+        }
+    }
+
+
+    void Awake()
+    {
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+		permissionList.Add(Permission.Microphone);         
+		permissionList.Add(Permission.Camera);               
+#endif
+        // keep this alive across scenes
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+    
+    
+    private void Start()
+    {
+        CheckAppId();
+    }
+    
+//Temporary button on Scene
+    public void JoinVideo()
+    {
+        OnJoinButtonClicked(true, false);
+    }
+    
+    private void Update()
+    {
+        CheckPermissions();
+    }
+    
+    /// <summary>
+    ///   Checks for platform dependent permissions.
+    /// </summary>
+    private void CheckPermissions()
+    {
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+        foreach(string permission in permissionList)
+        {
+            if (!Permission.HasUserAuthorizedPermission(permission))
+            {                 
+				Permission.RequestUserPermission(permission);
+			}
+        }
+#endif
+    }
+    
+    private void CheckAppId()
+    {
+        Debug.Assert(appID.Length > 10, "Please fill in your AppId first on Game Controller object.");
+    }
+    
+    // public void OnJoinAudience()
+    // {
+    //     // create app if nonexistent
+    //     if (ReferenceEquals(_controller, null))
+    //     {
+    //         // Quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+    //         _controller = new AgoraController(); // create app
+    //         // _controller.tmp = debugField;
+    //         _controller.LoadEngine(appID); // load engine
+    //     }
+    //
+    //     ChannelName = channelName;
+    //     _controller.JoinAudience(ChannelName);
+    //     
+    //         OnLevelFinishedLoading();
+    //     
+    //     // SceneManager.sceneLoaded += OnLevelFinishedLoading; // configure GameObject after scene is loaded
+    //     // SceneManager.LoadScene(_firstSceneName, LoadSceneMode.Single);
+    // }
+
+    public void OnJoinButtonClicked(bool enableVideo, bool muted = false)
+    {
+        // create app if nonexistent
+        if (ReferenceEquals(_controller, null))
+        {
+            Quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            var rotationQuad = Quad.transform.rotation;
+            rotationQuad =Quaternion.Euler(new Vector3(rotationQuad.x, rotationQuad.y, 180));
+            Quad.transform.rotation = rotationQuad;
+            _controller = new AgoraController(Quad); // create app
+            // _controller.tmp = debugField;
+            _controller.LoadEngine(appID); // load engine
+        }
+
+        ChannelName = channelName;
+        _controller.Join(ChannelName, appToken, enableVideo, muted);
+        
+        var currentScene = SceneManager.GetActiveScene();
+       
+            OnLevelFinishedLoading();
+        
+        // SceneManager.sceneLoaded += OnLevelFinishedLoading; // configure GameObject after scene is loaded
+        // SceneManager.LoadScene(PlaySceneName, LoadSceneMode.Single);
+    }
+
+    public void onLeaveButtonClicked()
+    {
+        if (!ReferenceEquals(_controller, null))
+        {
+            _controller.Leave(); // leave channel
+            _controller.UnloadEngine(); // delete engine
+            _controller = null; // delete app
+            // SceneManager.LoadScene(HomeSceneName, LoadSceneMode.Single);
+        }
+        Destroy(gameObject);
+    }
+    
+    public void OnLevelFinishedLoading()
+    {
+      
+            if (!ReferenceEquals(_controller, null))
+            {
+                _controller.OnSceneVideoLoaded(); // call this after scene is loaded
+            }
+            // SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+        
+    }
+    
+    private void OnApplicationPause(bool paused)
+    {
+        if (!ReferenceEquals(_controller, null))
+        {
+            _controller.EnableVideo(paused);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (!ReferenceEquals(_controller, null))
+        {
+            _controller.UnloadEngine();
+        }
+    }
+}
