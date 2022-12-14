f42a568296b1154360bd9723556dc2eb16b65f9e
Nitro <sayatraykulov@gmail.com>
2022-12-14 10:21:30 +0600

    Screenshot function added

--- Assets/Scripts/UI/UserButtonsView.cs
+++ Assets/Scripts/UI/UserButtonsView.cs
@@ -1,20 +1,18 @@
 using System;
 using System.Collections.Generic;
+using Assets.Scripts.UI;
 using Photon.Pun;
 using TMPro;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace Assets.Scripts.UI
+namespace UI
 {
     public class UserButtonsView : MonoBehaviourPunCallbacks
     {
         [SerializeField] private Button likes;
         [SerializeField] private TMP_Text likesCount;
-        [SerializeField] private Button videoSettings;
+        [SerializeField] private Button screenshot;
         [SerializeField] private Button playerSettings;
         [SerializeField] private Button quit;
         [SerializeField] private Button help;
@@ -24,10 +22,10 @@ namespace Assets.Scripts.UI
         [SerializeField] private Button chat;
         [SerializeField] private Button mobileUIToggle;
 
-        
+
         public Button Likes => likes;
         public TMP_Text LikesCount => likesCount;
-        public Button VideoSettings => videoSettings;
+        public Button Screenshot => screenshot;
         public Button PlayerSettings => playerSettings;
         public Button Quit => quit;
         public Button Help => help;
@@ -36,8 +34,16 @@ namespace Assets.Scripts.UI
         public Button CameraBtn => cameraBtn;
         public Button Chat => chat;
         public Button MobileUIToggle => mobileUIToggle;
-        
+
         private List<Button> _buttons = new List<Button>();
+
+
+        private const string Pattern = @"yyyyMMddHHmmssfff";
+
+        
+        
+        
+        
         private void Start()
         {
             InitialiseButtons();
@@ -46,26 +52,35 @@ namespace Assets.Scripts.UI
 
         private void InitialiseButtons()
         {
-           _buttons.Add(likes);
-           _buttons.Add(videoSettings);
-           _buttons.Add(playerSettings);
-           _buttons.Add(quit);
-           _buttons.Add(help);
-           _buttons.Add(more);
-           _buttons.Add(chat);
-           _buttons.Add(cameraBtn);
-           _buttons.Add(microphone);
-           _buttons.Add(mobileUIToggle);
+            _buttons.Add(likes);
+            _buttons.Add(screenshot);
+            _buttons.Add(playerSettings);
+            _buttons.Add(quit);
+            _buttons.Add(help);
+            _buttons.Add(more);
+            _buttons.Add(chat);
+            _buttons.Add(cameraBtn);
+            _buttons.Add(microphone);
+            _buttons.Add(mobileUIToggle);
 
-           foreach (var button in _buttons)
-           {
-              var controller = button.gameObject.AddComponent<BtnViewController>();
-              controller.CurrentButton = button;
-              controller.percentScale = 10f;
-           }
+            foreach (var button in _buttons)
+            {
+                var controller = button.gameObject.AddComponent<BtnViewController>();
+                controller.CurrentButton = button;
+                controller.percentScale = 10f;
+            }
 
-           Quit.onClick.AddListener(LeaveRoom);
-       
+            Screenshot.onClick.AddListener(TakeScreenshot);
+            Quit.onClick.AddListener(LeaveRoom);
+        }
+
+        private void TakeScreenshot()
+        {
+            var date = DateTime.Now.ToString(Pattern);
+            var path = Application.persistentDataPath + $"/{date}.png";
+            ScreenCapture.CaptureScreenshot(path);
+            Debug.Log($"Image saved in : {path}");
+            
         }
 
 
@@ -78,7 +93,10 @@ namespace Assets.Scripts.UI
         {
             // SceneManager.LoadSceneAsync(0);
             PhotonNetwork.LoadLevel(0);
+
+            // unsubscribe events
+            Screenshot.onClick.AddListener(TakeScreenshot);
+            Quit.onClick.AddListener(LeaveRoom);
         }
-        
     }
 }
