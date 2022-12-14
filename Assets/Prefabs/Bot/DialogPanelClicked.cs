d6bf6f181fd2f0d361261b455b970fa9c3f7c89e
Corvusincvr <Corvusincvr@gmail.com>
2022-12-14 12:55:47 +0600

    Bot logic

--- Assets/Prefabs/Bot/DialogPanelClicked.cs
+++ Assets/Prefabs/Bot/DialogPanelClicked.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogPanelClicked : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI textInDialog;
+    private const byte StartedDialogue = 0;
 
-    private string[] phrases = new[]
+    [SerializeField] private TextMeshProUGUI textInDialog;
+    [SerializeField] private Button _dialogPanel;
+
+    private byte _numDialog;
+
+    private readonly string[] _phrases = new[]
     {
-        "Hello",
-        "Welcome to Metaworld",
-        "My name is Android",
-        "How can I help you"
+        "Hello! ...",
+        "Welcome to MetaWorld! ...",
+        "My name is Android. ...",
+        "How can I help you? ..."
     };
+
+    private void Start()
+    {
+        _numDialog = StartedDialogue;
+        textInDialog.text = _phrases[_numDialog];
+        _dialogPanel.onClick.AddListener(ClickDialog);
+    }
+
+    private void ClickDialog()
+    {
+        if (_numDialog < _phrases.Length - 1)
+        {
+            _numDialog++;
+            textInDialog.text = _phrases[_numDialog];
+        }
+        else
+        {
+            _numDialog = 0;
+            textInDialog.text = _phrases[_numDialog];
+        }
+    }
 }
