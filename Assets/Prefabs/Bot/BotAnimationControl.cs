f51a092474fa7a444b729daa31644d7ca098bb11
Nitro <sayatraykulov@gmail.com>
2022-12-18 18:10:29 +0600

    BotAnimation script created

---
+++ Assets/Prefabs/Bot/BotAnimationControl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotAnimationControl : MonoBehaviour
+{
+    private int _animIDDance;
+    private int _animIDIsDancing;
+
+    private Animator _animator;
+    private bool _hasAnimator;
+}
