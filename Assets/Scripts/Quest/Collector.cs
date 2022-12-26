44b23c3fa385908b4047cb511476f4b057604c6b
Nitro <sayatraykulov@gmail.com>
2022-12-26 20:44:45 +0600

    10 coins for quest

---
+++ Assets/Scripts/Quest/Collector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Quest
+{
+    public class Collector : MonoBehaviour
+    {
+        private int _coinsSum;
+        private int _valuesSum;
+
+        public void AddCoin(int value)
+        {
+            _coinsSum++;
+            _valuesSum += value;
+            
+            Debug.Log($"You earned coin:\nCoins: {_coinsSum}\nValue: {_valuesSum}");
+        }
+        
+        
+        
+    }
+}
