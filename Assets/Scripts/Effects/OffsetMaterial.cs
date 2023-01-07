d3ffcf8db28838a9ea39d715f056e15b5af123b0
Nitro <sayatraykulov@gmail.com>
2023-01-07 18:32:36 +0600

    OffsetAnimation changed to DOTween

--- Assets/Scripts/Effects/OffsetMaterial.cs
+++ Assets/Scripts/Effects/OffsetMaterial.cs
@@ -1,24 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class OffsetMaterial : MonoBehaviour
+namespace Effects
 {
-    private Renderer _rend;
-    private Material _material;
-    private Vector2 _offsetBaseMap;
-    public float _speedX = -0.6f;
-    public float _speedY = -0.1f;
-    private void Start()
+    public class OffsetMaterial : MonoBehaviour
     {
-        _rend = GetComponent<Renderer> ();
-        _material = _rend.material;
-        _material.SetTextureOffset("_BaseMap", new Vector2(float.MaxValue,float.MaxValue));
-    }
-    
-    private void Update()
-    {
-        _offsetBaseMap = new Vector2(_offsetBaseMap.x + _speedX*Time.deltaTime, _offsetBaseMap.y + _speedY*_speedX*Time.deltaTime);
-        _material.SetTextureOffset("_BaseMap", _offsetBaseMap);
+        private Renderer _rend;
+        private Material _material;
+        private Vector2 _offsetBaseMap;
+        public float _speedX = -0.6f;
+        public float _speedY = -0.1f;
+
+        private void Start()
+        {
+            _rend = GetComponent<Renderer>();
+            _material = _rend.material;
+            var seq = DOTween.Sequence();
+            seq.Append(_material.DOOffset(new Vector2(1f, 0f), "_BaseMap", 2).SetEase(Ease.InCirc));
+            seq.Append(_material.DOOffset(new Vector2(0f, 1f), "_BaseMap", 3).SetEase(Ease.InSine));
+            seq.Append(_material.DOOffset(new Vector2(-1f, 0f), "_BaseMap", 2).SetEase(Ease.InBounce));
+            seq.Append(_material.DOOffset(new Vector2(0f, -1f), "_BaseMap", 3).SetEase(Ease.InElastic));
+            seq.SetLoops(-1);
+        }
     }
 }
